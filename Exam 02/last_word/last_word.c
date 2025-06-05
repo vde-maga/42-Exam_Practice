f1fe0cd85e6f74409337fc58c6e9d58c83457f1a
Miraiy <66632625+vde-maga@users.noreply.github.com>
2025-06-05 15:58:17 +0100

    Enunciados Colocados nos Exercicios

--- Exam 02/last_word/last_word.c
+++ Exam 02/last_word/last_word.c
@@ -49,3 +49,33 @@ int	main(int argc, char **argv)
 		last_word(argv[1]);
 	write(1, "\n", 1);
 }
+
+/*
+
+Assignment name  : last_word
+Expected files   : last_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its last word followed by a \n.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or there are no words, display a newline.
+
+Example:
+
+$> ./last_word "FOR PONY" | cat -e
+PONY$
+$> ./last_word "this        ...       is sparta, then again, maybe    not" | cat -e
+not$
+$> ./last_word "   " | cat -e
+$
+$> ./last_word "a" "b" | cat -e
+$
+$> ./last_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+
+*/
--- Exam 02/paramsum/paramsum.c
+++ Exam 02/paramsum/paramsum.c
@@ -30,3 +30,27 @@ int	main(int ac, char **av)
 		write(1, "0", 1);
 	write(1, "\n", 1);
 }
+
+/*
+
+Assignment name  : paramsum
+Expected files   : paramsum.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that displays the number of arguments passed to it, followed by
+a newline.
+
+If there are no arguments, just display a 0 followed by a newline.
+
+Example:
+
+$>./paramsum 1 2 3 5 7 24
+6
+$>./paramsum 6 12 24 | cat -e
+3$
+$>./paramsum | cat -e
+0$
+$>
+
+*/
