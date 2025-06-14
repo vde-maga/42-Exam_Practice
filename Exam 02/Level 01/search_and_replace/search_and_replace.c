702d7ab6c90ef38895341d9d15c1ef86b463db08
Miraiy <66632625+vde-maga@users.noreply.github.com>
2025-06-14 16:44:18 +0100

    fixed code

--- Exam 02/Level 01/search_and_replace/search_and_replace.c
+++ Exam 02/Level 01/search_and_replace/search_and_replace.c
@@ -14,7 +14,7 @@
 
 void	search_and_replace(char *str, char search, char replace)
 {
-	int	i;
+	int           i;
 
 	i = 0;
 	while (str[i])
@@ -29,7 +29,10 @@ void	search_and_replace(char *str, char search, char replace)
 int	main(int argc, char **argv)
 {
 	if (argc == 4)
-		search_and_replace(argv[1], argv[2][0], argv[3][0]);
+	{
+		if (!argv[2][1] && !argv[3][1])
+			search_and_replace(argv[1], argv[2][0], argv[3][0]);
+	}
 	write(1, "\n", 1);
 }
 
